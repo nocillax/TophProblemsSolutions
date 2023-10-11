ecc8a2e80e37e90484ac69c6bbbd0c5ee975df2c
Md Asif Chowdhury <asifjarif@gmail.com>
2023-10-11 23:34:24 +0600

    Create Fibonacci Numbers.cpp

---
+++ Fibonacci Numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	long long int arr[n];
+
+    arr[0] = 1;
+    arr[1] = 1;
+
+    for(int i=2; i<n; i++){
+        arr[i] = arr[i-1]+ arr[i-2];
+    }
+
+    cout<<arr[n-1]<<endl;
+	
+	return 0;
+}
