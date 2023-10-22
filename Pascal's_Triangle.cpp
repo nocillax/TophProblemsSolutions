f6524c02c7559f30418b2e41bd4389f5d2a57d11
Md Asif Chowdhury <asifjarif@gmail.com>
2023-10-23 05:27:48 +0600

    Create Pascal's_Triangle.cpp

---
+++ Pascal's_Triangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    
+    ll x;
+    cin>>x;
+    
+    ll res = pow(2,(x-1));
+    
+    cout<<res;
+    
+}
